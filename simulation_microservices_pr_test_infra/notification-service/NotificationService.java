c369fb7ac5acfd23f4470bba0bdfd8918c118536
Slimani Sara <selimanisara2@gmail.com>
2025-12-28 21:14:26 +0100

    first commit pr le test du kafka

---
+++ simulation_microservices_pr_test_infra/notification-service/NotificationService.java
@@ -0,0 +1,6 @@
+public class NotificationService {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Notification Service démarré");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
---
+++ simulation_microservices_pr_test_infra/payment-service/PaymentService.java
@@ -0,0 +1,6 @@
+public class PaymentService {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Payment Service démarré");
+        Thread.sleep(Long.MAX_VALUE); // garde le service en marche
+    }
+}
